1806a428cf925fde73b30492b0b074018221ddde
luisferro2 <a01378248@itesm.mx>
2021-06-07 23:24:17 -0500

    Hacer la expresión regular con grupos de captura para que pueda detectar los primeros 5 grupos y el proceso para convertir la string en un html iniciado.

---
+++ python-file.py
@@ -0,0 +1,13 @@
+# This file is a meme.
+
+my_variable = f'''string that represents
+		  the capacity of python to 
+		  format multiline strings {}'''
+
+if True:
+    print("""Increible
+	     \072 la realidad"""
+else:
+    pass
+
+my_byte = B"this is a byte"
